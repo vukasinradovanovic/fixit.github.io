05b20c4acd2d2da666a577ba5d5e87782706afc0
Vukasin Radovanovic <vukasinradovanovic@gmail.com>
2025-08-17 00:53:09 +0200

    documentation fix

--- public/js/partials/footer.js
+++ public/js/partials/footer.js
@@ -22,7 +22,7 @@ export function initFooter() {
         {
             name: "doc",
             text: "DOKUMENTACIJA",
-            slug: "img/dokumentacija.pdf"
+            slug: "dokumentacija.pdf"
         },
     ]
 
